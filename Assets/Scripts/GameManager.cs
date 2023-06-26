f85ace5d9d6cf8a8b3026838944cde8bd26d462c
IlJeko <60620724+IlJeko@users.noreply.github.com>
2023-06-26 16:57:15 +0200

    Scaffolded GameManager + modified scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int playerScore = 0;
+    public static int opponentScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // add here invoke ball spawn
+    }
+
+    public static void Score (string wallId)
+    {
+
+    } 
+
+    public static void GameOver()
+    {
+
+    }
+}
