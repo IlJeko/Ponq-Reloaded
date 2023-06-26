42e9775d5b4e41a6fa5422966fd3520abe3a93dd
IlJeko <60620724+IlJeko@users.noreply.github.com>
2023-06-26 17:08:38 +0200

    Added player control + fix walls components

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float speed = 10.0f;
+    private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = body.velocity;
+        if (Input.GetKey(moveUp))
+        {
+            velocity.z = speed;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            velocity.z = -speed;
+        }
+        else
+        {
+            velocity.z = 0;
+        }
+        body.velocity = velocity;
+    }
+}
