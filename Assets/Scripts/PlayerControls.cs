3a2439e5d145ccda4871e3fa7d5dfe49e779cc97
IlJeko <60620724+IlJeko@users.noreply.github.com>
2023-06-28 17:13:16 +0200

    Fixed player controller

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    public KeyCode moveUp = KeyCode.W;
-    public KeyCode moveDown = KeyCode.S;
     public float speed = 10.0f;
-    private Rigidbody body;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        body = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 velocity = body.velocity;
-        if (Input.GetKey(moveUp))
-        {
-            velocity.z = speed;
-        }
-        else if (Input.GetKey(moveDown))
-        {
-            velocity.z = -speed;
-        }
-        else
-        {
-            velocity.z = 0;
-        }
-        body.velocity = velocity;
+        float inputSpeed = 0f;
+        inputSpeed = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(0f, 0f, inputSpeed * speed * Time.deltaTime);
     }
 }
