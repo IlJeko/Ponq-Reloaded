5319d3756a673393c25408120fc3000444bf8bb8
IlJeko <60620724+IlJeko@users.noreply.github.com>
2023-07-02 20:27:13 +0200

    Added basic UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,21 +7,48 @@ public class GameManager : MonoBehaviour
     public static int playerScore = 0;
     public static int opponentScore = 0;
     public static GameObject ball;
+    public GUISkin layout;
 
     // Start is called before the first frame update
     void Start()
     {
         ball = GameObject.FindGameObjectWithTag("Ball");
-        // add here invoke ball spawn
     }
 
-    public static void Score (string wallId)
+    public static void Score (string goalId)
     {
+        if (goalId == "PlayerGoal")
+        {
+            playerScore++;
+        }
+        else
+        {
+            opponentScore++;
+        }
+    }
 
-    } 
-
-    public static void GameOver()
+    void OnGUI()
     {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + playerScore);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + opponentScore);
 
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART"))
+        {
+            playerScore = 0;
+            opponentScore = 0;
+            ball.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+        }
+
+        if (playerScore == 5)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (opponentScore == 10)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
     }
 }
