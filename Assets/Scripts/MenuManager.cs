0bc0445cf0614e8c7d93aef0ab9b978ddcc67995
IlJeko <60620724+IlJeko@users.noreply.github.com>
2023-07-02 23:39:48 +0200

    Added menu logic

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Level1() => SceneManager.LoadScene("IcyLevel");
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #endif
+    }
+}
